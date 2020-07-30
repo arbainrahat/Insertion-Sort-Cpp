7493646b7623c7ba9279c9d6e0c76047fcf93175
arbainrahat <68120270+arbainrahat@users.noreply.github.com>
2020-07-30 23:36:01 +0500

    Add files via upload

---
+++ Insertion_Sort(Decending Order).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Enter Array\n";
+	int a[5];
+	int i,j,t;
+	for(int l=0;l<5;l++){
+		cin>>a[l];
+	}
+	for(i=0;i<=4;i++){
+		j=i;
+		t=a[j];
+		while(j>0 && t>a[j-1]){
+			a[j]=a[j-1];
+			j=j-1;
+		}
+		a[j]=t;
+	}
+	cout<<"Insertion Sort(Decending Order)\n";
+	for(int l=0;l<5;l++){
+		cout<<a[l]<<"	";
+	}
+	cin.get();
+	system("pause");
+}
